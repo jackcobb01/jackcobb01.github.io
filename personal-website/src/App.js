878afd82da8b0d28a6994de1fbbea113fee556c7
jackcobb01 <jackrc931@gmail.com>
2021-07-18 17:47:06 -0400

    pages now have their own paths

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -2,6 +2,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
+import { BrowserRouter as Switch, Router, Route } from "react-router-dom";
 import MainHeader from './components/MainHeader.js';
 import HomePage from './views/HomePage.js';
 import AboutPage from './views/AboutPage.js';
@@ -11,39 +12,40 @@ import ContactPage from './views/ContactPage.js';
 // this function returns the app
 function App() {
 
-  // state variables:
-  const [pageSelection, setPageSelection] = useState(0);
-
-  function changePageSelection(pageType) {
-    setPageSelection(pageType);
+  // setting up main header button selection:
+  var pageSelection = [false, false, false, false];
+  if (window.location.pathname === "/") {
+    pageSelection[0] = true;
+  }
+  else if (window.location.pathname === "/About") {
+    pageSelection[1] = true;
+  }
+  else if (window.location.pathname === "/Projects") {
+    pageSelection[2] = true;
+  }
+  else {
+    pageSelection[3] = true;
   }
 
   return (
     <div className="App">
       {/*Main header component for the website*/}
-      <MainHeader pageSelection={changePageSelection} />
-
+      <MainHeader pageSelection={pageSelection} />
       {/*Page component that is based on which page is selected*/}
-      {pageSelection === 0?
-        <HomePage />
-        :
-        null
-      }
-      {pageSelection === 1?
-        <AboutPage />
-        :
-        null
-      }
-      {pageSelection === 2?
-        <ProjectPage />
-        :
-        null
-      }
-      {pageSelection === 3?
-        <ContactPage />
-        :
-        null
-      }
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/About">
+          <AboutPage />
+        </Route>
+        <Route path="/Projects">
+          <ProjectPage />
+        </Route>
+        <Route path="/Contact">
+          <ContactPage />
+        </Route>
+      </Switch>
       {/*Given starter code from react- WILL REMOVE*/}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
