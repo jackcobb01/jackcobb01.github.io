069d9d53d334ce67098a24e4a39b986645f023e6
Jack Cobb <jack@Jacks-MacBook-Air.local>
2022-01-11 00:26:30 -0500

    began frontend of contact page

--- personal-website/src/views/ContactPage.js
+++ personal-website/src/views/ContactPage.js
@@ -1,5 +1,5 @@
 // imports
-import React from 'react';
+import React, {useState} from 'react';
 import Title from '../components/Title.js';
 import '../styles/ContactPage.css';
 
@@ -7,11 +7,25 @@ import '../styles/ContactPage.css';
 function ContactPage(props) {
 
     // state variables:
+    const [pictureCounter, setPictureCounter] = useState(0);
   
     // return statement
     return (
         <div className="ContactPage">
             <Title titleText="Contact Me" />
+
+            <div className="ContactBox">
+                <div className="messageInformation">
+                    <div className="MessageInputDressing" />
+                        <div className="messageInputs">
+                            <div className="messageInputHeader">Subject</div>
+                            <div className="messageInputHeader">Body</div>
+                        </div>
+                    <div className="MessageInputDressing" />
+                </div>
+                
+                <div className="sendMessageButton" onClick={() => setPictureCounter(pictureCounter + 1)} >Send {pictureCounter}</div>
+            </div> 
         </div>
     );
   
