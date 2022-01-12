6d4f56028bcca2a83ae1db962efe966a31f698c9
Jack Cobb <jack@Jacks-MacBook-Air.local>
2022-01-11 21:57:27 -0500

    finished contact page frontend and email

--- personal-website/src/views/ContactPage.js
+++ personal-website/src/views/ContactPage.js
@@ -7,25 +7,37 @@ import '../styles/ContactPage.css';
 function ContactPage(props) {
 
     // state variables:
-    const [pictureCounter, setPictureCounter] = useState(0);
+    const [subjectName, setSubjectName] = useState("");
+    const [bodyText, setBodyText] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (subjectName !== "" && bodyText !== "") {
+            //alert(`Submitting ${subjectName} --- ${bodyText}`)
+            window.location = `mailto:jackrc931@gmail.com?subject=${encodeURIComponent(subjectName)}&body=${encodeURIComponent(bodyText)}`
+        }
+    }
   
     // return statement
     return (
         <div className="ContactPage">
             <Title titleText="Contact Me" />
 
-            <div className="ContactBox">
-                <div className="messageInformation">
-                    <div className="MessageInputDressing" />
-                        <div className="messageInputs">
-                            <div className="messageInputHeader">Subject</div>
-                            <div className="messageInputHeader">Body</div>
-                        </div>
-                    <div className="MessageInputDressing" />
-                </div>
-                
-                <div className="sendMessageButton" onClick={() => setPictureCounter(pictureCounter + 1)} >Send {pictureCounter}</div>
-            </div> 
+            <form className="ContactBox" onSubmit={handleSubmit}>
+                    <div className="messageInformation">
+                        <div className="MessageInputDressing" />
+                            <div className="messageInputs">
+                                <div className="messageInputHeader">Subject</div>
+                                <input className="messageSubjectInput" type="text" value={subjectName} onChange={e => setSubjectName(e.target.value)}/>
+                                <div className="messageInputHeader">Body</div>
+                                <textarea className="messageBodyInput" value={bodyText} onChange={e => setBodyText(e.target.value)}/>
+                            </div>
+                        <div className="MessageInputDressing" />
+                    </div>
+                    <input className="sendMessageButton" type="submit" value="Send"/>
+            </form> 
+
+            <div>{subjectName}</div>
         </div>
     );
   
